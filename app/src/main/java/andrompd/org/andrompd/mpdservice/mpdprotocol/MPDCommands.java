413a7c708786cb65907579efef192a9b33e055b7
Hendrik Borghorst <hendrikborghorst@gmail.com>
2016-08-14 16:36:03 +0200

    Fix Bob Dylans "Love and Theft" album retrieval

--- app/src/main/java/andrompd/org/andrompd/mpdservice/mpdprotocol/MPDCommands.java
+++ app/src/main/java/andrompd/org/andrompd/mpdservice/mpdprotocol/MPDCommands.java
@@ -29,15 +29,15 @@ public class MPDCommands {
 
 
     public static String MPD_COMMAND_REQUEST_ARTIST_ALBUMS(String artistName) {
-        return "list album \"" + artistName + "\"";
+        return "list album \"" + artistName.replaceAll("\"","\\\\\"") + "\"";
     }
 
     public static String MPD_COMMAND_REQUEST_ARTIST_ALBUMS_WITH_MBID(String artistName) {
-        return "list album artist \"" + artistName + "\" group MUSICBRAINZ_ALBUMID";
+        return "list album artist \"" + artistName.replaceAll("\"","\\\\\"") + "\" group MUSICBRAINZ_ALBUMID";
     }
 
     public static String MPD_COMMAND_REQUEST_ALBUM_TRACKS(String albumName) {
-        return "find album \"" + albumName + "\"";
+        return "find album \"" + albumName.replaceAll("\"","\\\\\"") + "\"";
     }
 
     public static final String MPD_COMMAND_REQUEST_ARTISTS = "list artist";
