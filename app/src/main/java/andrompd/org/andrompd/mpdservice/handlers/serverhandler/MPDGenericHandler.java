e95b438fc47ed712fb8ccfef61253eb19cf0d633
Hendrik Borghorst <hendrikborghorst@gmail.com>
2016-08-16 19:18:14 +0200

    Document MPDGenericHandlre

--- app/src/main/java/andrompd/org/andrompd/mpdservice/handlers/serverhandler/MPDGenericHandler.java
+++ app/src/main/java/andrompd/org/andrompd/mpdservice/handlers/serverhandler/MPDGenericHandler.java
@@ -29,13 +29,44 @@ import andrompd.org.andrompd.mpdservice.handlers.MPDConnectionStateChangeHandler
 import andrompd.org.andrompd.mpdservice.handlers.responsehandler.MPDResponseHandler;
 import andrompd.org.andrompd.mpdservice.mpdprotocol.MPDConnection;
 
+/**
+ * This class is a base class for all derived handlers that talk to the MPD server.
+ * There are three handlers for different tasks.
+ *
+ *  - MPDCommandHandler - Issues all playback commands to the mpd server which are handled instantly.
+ *                      - For example: play,pause,stop ...
+ *
+ *  - MPDQueryHandler   - Handles all requests to the database and other state objects of the MPD server.
+ *                      - For example: Album, artist, playlists, output list ...
+ *  - MPDStateMonitoringHandler - Monitors the state and notifies connected callback listeners in a certain
+ *                                interval about changes in state. It uses the idle state of the MPDConnection
+ *                                as a notification about changes at the server side.
+ *
+ * All handlers are static singletons and run in a new spawned thread. You can get the singleton via
+ * an static method. Each handler has one MPDConnection object, that is used for talking to the mpd server.
+ *
+ */
 public abstract class MPDGenericHandler extends Handler implements MPDConnection.MPDConnectionStateChangeListener {
 
+    /**
+     * MPDConnetion used for the communication to the MPD server.
+     */
     protected MPDConnection mMPDConnection;
 
+
+    /**
+     * List of handlers that get notified when there is a change of state for the network connection
+     * (connected, disconnected)
+     */
     protected ArrayList<MPDConnectionStateChangeHandler> mConnectionStateListener;
 
 
+    /**
+     * Protected constructor that has to be called from subclasses. If it is not called
+     * the MPDConnection objcet is not ready for use and the class will cause a crash.
+     * @param looper Looper that is used by this handler. Needs to be created by the
+     *               subclass otherwise the network communication is not handled in a separate thread.
+     */
     protected MPDGenericHandler(Looper looper) {
         super(looper);
         mMPDConnection = new MPDConnection();
@@ -55,50 +86,69 @@ public abstract class MPDGenericHandler extends Handler implements MPDConnection
     public void handleMessage(Message msg) {
         super.handleMessage(msg);
 
+        // Check if the attached object is of the correct type.
         if (!(msg.obj instanceof MPDHandlerAction)) {
             /* Check if the message object is of correct type. Otherwise just abort here. */
             return;
         }
 
+        // Cast the message object to the MPDHandlerAction class to use its properties.
         MPDHandlerAction mpdAction = (MPDHandlerAction) msg.obj;
+
         /* Catch MPD exceptions here for now. */
-        MPDResponseHandler responseHandler;
+
+        // Get the set action for this message object and handle it accordingly.
+        // Allowed actions are defined in the MPDHandlerAction class.
+
+        // This class only handles base actions as more defined actions are only handled
+        // in the subclasses.
+        // Usually the pattern of handling is the same. Get the necessary extras out of the Action objcet
+        // and then call the mMPDConnection with the correct method
         MPDHandlerAction.NET_HANDLER_ACTION action = mpdAction.getAction();
+        // Set the server parameters (hostname, password, port)
         if (action == MPDHandlerAction.NET_HANDLER_ACTION.ACTION_SET_SERVER_PARAMETERS) {
             /* Parse message objects extras */
+            // Get the message extras
             String hostname = mpdAction.getStringExtra(MPDHandlerAction.NET_HANDLER_EXTRA_STRING.EXTRA_SERVER_HOSTNAME);
             String password = mpdAction.getStringExtra(MPDHandlerAction.NET_HANDLER_EXTRA_STRING.EXTRA_SERVER_PASSWORD);
             Integer port = mpdAction.getIntExtra(MPDHandlerAction.NET_HANDLER_EXTRA_INT.EXTRA_SERVER_PORT);
             if ((null == hostname) || (null == port)) {
                 return;
             }
-
             mMPDConnection.setServerParameters(hostname, password, port);
         } else if (action == MPDHandlerAction.NET_HANDLER_ACTION.ACTION_CONNECT_MPD_SERVER) {
+            // Connect to the mpd server. Server parameters have to be set before.
             try {
                 mMPDConnection.connectToServer();
             } catch (IOException e) {
+                // If connection failed notify observers about the disconnect.
                 onDisconnected();
             }
         } else if (action == MPDHandlerAction.NET_HANDLER_ACTION.ACTION_DISCONNECT_MPD_SERVER) {
+            // Disconnect from the mpd server.
             mMPDConnection.disconnectFromServer();
         }
     }
 
-    public void internalRegisterConnectionStateListener(MPDConnectionStateChangeHandler stateHandler) {
+    // Internal class that registers observers to the state changes. This is only used by the subclasses.
+    protected void internalRegisterConnectionStateListener(MPDConnectionStateChangeHandler stateHandler) {
         if (null == stateHandler) {
             return;
         }
         mConnectionStateListener.add(stateHandler);
     }
 
-    public void internalUnregisterConnectionStateListener(MPDConnectionStateChangeHandler stateHandler) {
+    // Internal class that unregisters observers to the state changes. This is only used by the subclasses.
+    protected void internalUnregisterConnectionStateListener(MPDConnectionStateChangeHandler stateHandler) {
         if (null == stateHandler) {
             return;
         }
         mConnectionStateListener.remove(stateHandler);
     }
 
+    /**
+     * This method will notify all registered observers about the connected state
+     */
     @Override
     public void onConnected() {
         // Send a message to all registered listen handlers.
@@ -109,6 +159,9 @@ public abstract class MPDGenericHandler extends Handler implements MPDConnection
         }
     }
 
+    /**
+     * This method will notify all registered observers about the disconnected state
+     */
     @Override
     public void onDisconnected() {
         // Send a message to all registered listen handlers.
