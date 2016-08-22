fd18771ca1692792d6b0b16e268f9d7c8333126f
Hendrik Borghorst <hendrikborghorst@gmail.com>
2016-08-23 00:02:51 +0200

    Comments on the FilesLoader

--- app/src/main/java/andrompd/org/andrompd/application/loaders/FilesLoader.java
+++ app/src/main/java/andrompd/org/andrompd/application/loaders/FilesLoader.java
@@ -10,20 +10,35 @@ import andrompd.org.andrompd.mpdservice.handlers.responsehandler.MPDResponseFile
 import andrompd.org.andrompd.mpdservice.handlers.serverhandler.MPDQueryHandler;
 import andrompd.org.andrompd.mpdservice.mpdprotocol.mpdobjects.MPDFileEntry;
 
+/**
+ * Loads a list of files, directories and playlists from the MPDQueryHandler
+ */
 public class FilesLoader extends Loader<List<MPDFileEntry>> {
     private FilesResponseHandler mFilesResponseHandler;
 
+    /**
+     * Used context
+     */
     Context mContext;
+
+    /**
+     * Path to request file entries from
+     */
     String mPath;
 
     public FilesLoader(Context context, String path) {
         super(context);
         mContext = context;
         mPath = path;
+
+        // Response handler for receiving the file list asynchronously
         mFilesResponseHandler = new FilesResponseHandler();
     }
 
 
+    /**
+     * Delivers the results to the GUI thread
+     */
     private class FilesResponseHandler extends MPDResponseFileList {
         @Override
         public void handleTracks(List<MPDFileEntry> fileList, int start, int end) {
@@ -32,21 +47,34 @@ public class FilesLoader extends Loader<List<MPDFileEntry>> {
     }
 
 
+    /**
+     * Start the loader
+     */
     @Override
     public void onStartLoading() {
         forceLoad();
     }
 
+
+    /**
+     * Stop the loader
+     */
     @Override
     public void onStopLoading() {
 
     }
 
+    /**
+     * Reset the data with an empty data set
+     */
     @Override
     public void onReset() {
         deliverResult(null);
     }
 
+    /**
+     * Requests the file list from the MPDQueryHandler, it will respond asynchronously
+     */
     @Override
     public void onForceLoad() {
         MPDQueryHandler.getFiles(mFilesResponseHandler,mPath);
